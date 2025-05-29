6e9a304ce6a26fc4bfe861e640ac6c4105c432bc
Pawel <pawel.koscielinski-extern@retresco.de>
2025-05-29 12:40:15 +0200

    prettier added

---
+++ angular-live-coding/src/app/components/edit-user/edit-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-user',
+  imports: [],
+  templateUrl: './edit-user.component.html',
+  styleUrl: './edit-user.component.scss',
+  standalone: true,
+})
+export class EditUserComponent {}
